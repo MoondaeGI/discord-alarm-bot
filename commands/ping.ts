12cdddb02b01579b22b23cdbba472c44f778a1c7
MoondaeGI <ansgur119977@gmail.com>
2025-12-13 23:27:27 +0900

    251213 ping-pong event 추가

--- commands/ping.ts
+++ commands/ping.ts
@@ -5,8 +5,8 @@ export const pingCommand = {
   name: 'ping',
   description: 'Ping the bot',
   execute: async (interaction: ChatInputCommandInteraction) => {
-    const message = `나나미짱 살아있어요! 응답속도: ${Date.now() - interaction.createdTimestamp}ms`;
-    await interaction.reply(message);
-    logInfo(message);
+    const duration = Date.now() - interaction.createdTimestamp;
+    await interaction.reply(`나나미짱 살아있어요! 응답속도: ${duration}ms`);
+    logInfo(`[PING] ${duration}ms`);
   },
 };
